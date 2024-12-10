e7246211df47c3f8f9d2dc30cfacdda53146baaf
louiscsq <louiscsq>
2024-12-10 04:58:45 +0000

    add fake unit test

--- unit-tests/test_with_percent_run.py
+++ unit-tests/test_with_percent_run.py
@@ -1,2 +1,26 @@
 # Databricks notebook source
 # MAGIC %pip install -U nutter chispa
+
+# COMMAND ----------
+
+# https://github.com/microsoft/nutter
+from runtime.nutterfixture import NutterFixture, tag
+# https://github.com/MrPowers/chispa
+from chispa.dataframe_comparer import *
+
+
+class TestPercentRunFixture(NutterFixture):
+  def assertion_upper_columns_percent_run(self):
+    cols = ["col1", "col2", "col3"]
+    df = spark.createDataFrame([("abc", "cef", 1)], cols)
+    expected_df = spark.createDataFrame([("abc", "cef", 1)], cols)
+    assert_df_equality(upper_df, expected_df)
+
+# COMMAND ----------
+
+result = TestPercentRunFixture().execute_tests()
+result.exit(dbutils)
+
+# COMMAND ----------
+
+
